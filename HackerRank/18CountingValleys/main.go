57ad001e5c96b601de70d8b0b5ee77c432c9089a
Gongbing Hong <hgbbus@users.noreply.github.com>
2026-05-21 11:54:11 -0400

    Solved counting valleys

---
+++ HackerRank/18CountingValleys/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func countingValleys(steps int, path string) int {
+	altitude := 0
+	valleys := 0
+
+	for i := 0; i < len(path); i++ {
+		switch path[i] {
+		case 'U':
+			altitude++
+		case 'D':
+			if altitude == 0 {
+				valleys++
+			}
+			altitude--
+		}
+	}
+
+	return valleys
+}
+
+func main() {
+	var steps int
+	fmt.Scanln(&steps)
+
+	var path string
+	fmt.Scanln(&path)
+
+	result := countingValleys(steps, path)
+	fmt.Println(result)
+}
